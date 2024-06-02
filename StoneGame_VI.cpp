129ae714a6f64c733884ad441b47096e08bdb12a
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-06-02 13:01:09 +0530

    Update StoneGame_VI.cpp

--- StoneGame_VI.cpp
+++ StoneGame_VI.cpp
@@ -9,10 +9,7 @@ public:
         sort(v.begin(),v.end());//sorting vector's elements
         while(i>=0){//Traversing through Vector's initial size
             a1+=a[v[i][1]];i--;//Incrementing vector nd decrementing vector's indx 
-            if(i>=0)a2+=b[v[i][1]]; i--;//adding vector's initial elements decrementing at its  each pos
-        }
+            if(i>=0)a2+=b[v[i][1]]; i--;}//adding vector's initial elements decrementing at its  each pos
         if(a1>a2)return 1;//printing 1 to Winning game 
         else if(a1==a2)return 0;//printing 0 tot the eqaul score
-        else return -1;//printing -1 to the UnWinning Game Score 
-    }
-};
+        else return -1;}};//printing -1 to the UnWinning Game Score 
